5a829ba282c55ec9d1992dd9dffe21565e08725d
Hanif25-github <70031945+Hanif25-github@users.noreply.github.com>
2025-04-18 18:09:34 +0530

    Registration for User Details

--- src/main/java/com/example/mtb/MovieTicketBookingSystemApiApplication.java
+++ src/main/java/com/example/mtb/MovieTicketBookingSystemApiApplication.java
@@ -2,6 +2,7 @@ package com.example.mtb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 public class MovieTicketBookingSystemApiApplication {
