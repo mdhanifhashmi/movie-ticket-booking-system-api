3108c613af50bcad0d128848052981de87cf12a9
Hanif25-github <70031945+Hanif25-github@users.noreply.github.com>
2025-04-30 15:48:02 +0530

    Movie entity and static movie data

---
+++ src/main/java/com/example/mtb/enums/Genre.java
@@ -0,0 +1,22 @@
+package com.example.mtb.enums;
+
+public enum Genre {
+    ACTION,
+    ADVENTURE,
+    COMEDY,
+    DRAMA,
+    HORROR,
+    THRILLER,
+    CRIME,
+    ROMANCE,
+    SCIENCE_FICTION,
+    DOCUMENTARY,
+    HISTORICAL,
+    BIOGRAPHY,
+    WAR,
+    ANIMATION,
+    FANTASY,
+    MYSTERY,
+    MUSICAL,
+    FAMILY
+}
